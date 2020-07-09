2b0eb676c20a3310b5cf6e6fd8046728066cffda
David Jimenez <runnerdave@gmail.com>
2020-07-09 21:14:50 +1000

    Initial commit

---
+++ src/test/java/net/runnerdave/springbootApp/SpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.runnerdave.springbootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
